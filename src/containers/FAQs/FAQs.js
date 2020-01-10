5ec387b4bb1ddbc8d21a152379052c81eae56c45
aosante <andres.osante@proximitycr.com>
2020-01-10 14:47:08 -0600

    completed FAQs and landing page adjustments

--- src/containers/FAQs/FAQs.js
+++ src/containers/FAQs/FAQs.js
@@ -27,113 +27,105 @@ const FAQs = () => {
         </LayoutWrapperTopbar>
         <LayoutWrapperMain>
           <div className={css.content}>
-            <h1 className={css.title}>FAQs</h1>
-            <div className={css.section}>
-              <h2 className={css.sectionTitle}>About Us</h2>
-              <ul className={css.questionList}>
-                <li>Where is Swoptop available? </li>
+            <h1 className={css.title}>FAQs: Renter</h1>
+            <section className={css.section}>
+              <div className={css.col}>
+                <h2>How long unitl my rental request is accepted?</h2>
                 <p>
-                  Swoptop is currently available at The University of Alabama in Tuscaloosa, AL.
+                  The Owner has up until the day before the rental to accept or deny your request.
                 </p>
-                <li>How much is the Swoptop fee?</li>
-                <p>We charge a 15% fee to the owner & a 10% fee to the renter.</p>
-              </ul>
-            </div>
-            <hr />
-            <div className={css.section}>
-              <h2 className={css.sectionTitle}>Renter Questions</h2>
-              <ul className={css.questionList}>
-                <li>When will I be charged for an item?</li>
+                <h2>How does cleaning an item work?</h2>
                 <p>
-                  The payment method linked to your Swoptop account will be charged once your rental
-                  request is accepted by the owner.{' '}
+                  Owner’s are responsible for cleaning their items. Please return the item as is
+                  (unwashed).
                 </p>
-                <li>How do I receive an item?</li>
+                <h2>Can I rent from someone that does not go to my school?</h2>
                 <p>
-                  Once the rental request is accepted, you are sent the Owner’s phone number via
-                  email. Therefore, you text each other to set up a time for pickup.{' '}
+                  No. At the moment, rentals are only available for students that attend The
+                  University of Alabama. If you would like Swoptop on your campus, sign up for the
+                  waitlist!
                 </p>
-                <li>How does cleaning an item work?</li>
-                <p>Owner’s are responsible for cleaning their items. Return the item as is.</p>
-                <li>How do I return an item?</li>
-                <p>
-                  Text the Owner to set up a time to return the item on the last day of your rental
-                  period (day 3).
-                </p>
-                <li>How long is the rental period?</li>
-                <p>The rental period is 3 days.</p>
-                <li>What is the Swoptop fee for the Renter? </li>
-                <p>10% of the transaction</p>
-                <li>Is there a late fee?</li>
-                <p>
-                  Yes. If you fail to return the item by the end of the third day (last day of the
-                  rental period) you will be charged a $10/day late fee.{' '}
-                </p>
-                <li>What if I accidentally damage a rented item?</li>
-                <p>
-                  Reach out by clicking “Contact Us” on the home page or DM us on Instagram if you
-                  damage an item. We will reach out to the Owner on your behalf. Damage fee is
-                  considered on a case by case basis depending on the item and damage committed. If
-                  the damage is beyond repair, you will be charged the damage cost for this item.
-                </p>
-                <li>Can I rent from someone that doesn’t go to my school? </li>
-                <p>
-                  At the moment, rentals are only available for students that attend The University
-                  of Alabama. .
-                </p>
-                <li>Can I request to rent items when I’m not physically at school? </li>
+                <h2>Can I request to rent an item when I am not at school?</h2>
                 <p>
                   Yes! You can browse from anywhere and request to rent items when you are not at
-                  school as long as you set the pick up date for when you return to campus.
+                  school as long as the rental period begins when you are back at school.
                 </p>
-                <li>What happens if an item I rented isn’t in good condition?</li>
+              </div>
+              <div className={css.col}>
+                <h2>How do I get in contact with the Owner?</h2>
                 <p>
-                  Reach out by clicking “Contact Us” on the home page or DM us on Instagram if this
-                  happens. Our owners are held to high standards, and although this rarely occurs,
-                  we will make sure it is addressed. Each situation will be handled on a case by
-                  case basis.{' '}
+                  Once the Owner accepts your rental request, you will be emailed their phone
+                  number. From there, you can reach out and find a time to meet up to exchange the
+                  item on the first day of the rental period as well as on the third day of the
+                  rental period when its time to return the item.
                 </p>
-                <li>How do I know that the owner is reliable?</li>
+                <h2>What happens if I accidentally damage an item?</h2>
                 <p>
-                  We take the security of our platform very seriously, therefore we ask you to
-                  qualify if the exchange has been done in a timely manner and if the item is given
-                  to you in good condition. We monitor our users based on those qualifications and
-                  more. If anyone is not up to the standards of our platform they will be removed
-                  from the platform.
+                  Reach out by clicking “Contact Us” on the home page if you damage an item. We will
+                  reach out to the Owner on your behalf. Damage fee’s are issued depending on the
+                  damage committed. In other words, if the damage is beyond repair, you will be
+                  charged the damage cost for this item.
                 </p>
-                <li>How do I keep track of the items that I like?</li>
+              </div>
+              <div className={css.col}>
+                <h2>What is the Swoptop fee for Renters?</h2>
+                <p>There is a 10% transaction fee each time you complete a rental.</p>
+                <h2>
+                  What happens if I am unable to return the item by the end of the rental period?
+                </h2>
                 <p>
-                  When you find an item that you love, you can favorite it by clicking the heart
-                  icon on the top of an image.
+                  First, out of courtesy please reach out to the Owner to let them know. If you fail
+                  to return the item by the end of the third day of the rental period you will be
+                  charged a $10/day late fee.
                 </p>
-              </ul>
-            </div>
-            <hr />
+                <h2>What happens if an item I rented is not in good condition?</h2>
+                <p>
+                  First, out of courtesy please reach out to the Owner to let them know. If you fail
+                  to return the item by the end of the third day of the rental period you will be
+                  charged a $10/day late fee.
+                </p>
+                <h2>What happens if an item I rented is not in good condition?</h2>
+                <p>
+                  Reach out by clicking “Contact Us” on the home page if this happens. Our owners
+                  are held to high standards, and although this rarely occurs, we will make sure it
+                  is addressed. Each situation will be handled on a case by case basis.
+                </p>
+              </div>
+            </section>
+            <h1 className={css.title}>FAQs: Owner</h1>
             <div className={css.section}>
-              <h2 className={css.sectionTitle}>Owner Questions</h2>
-              <ul className={css.questionList}>
-                <li>How do I know how much to price an item?</li>
+              <div className={css.col}>
+                <h2>How do I know how much to price an item?</h2>
                 <p>
                   We recommend you check how similar items are being priced on the marketplace. If
-                  you need help, DM us or use the “Contact Us” form on the home page and we can
+                  you need help, use the “Contact Us” form on the home page to reach out so we can
                   offer suggestions.
                 </p>
-                <li>How do I get paid?</li>
+                <h2>What is the Swoptop fee for Owners?</h2>
+                <p>There is a 15% transaction fee every time a rental is processed.</p>
+              </div>
+              <div className={css.col}>
+                <h2>How long does it take for my listing to be approved?</h2>
                 <p>
-                  All earnings will be transferred to your linked bank account within 7-10 business
-                  days depending on your bank.
+                  Our team checks new listings frequently and will do our best to accept within 2
+                  hours of creating the listing. If your listing does not meet our standards it will
+                  not post. Make sure to include high quality pictures.
                 </p>
-                <li>
-                  After signing up and posting my listing, how long do I have to wait for it to
-                  post?
-                </li>
+                <h2>How does exchanging an item work?</h2>
                 <p>
-                  Our team checks new listings frequently and will do our best to accept or make
-                  edits typically within 2 hours of creating the listing.
+                  Once you accept the rental request, the Renter will be emailed your phone number.
+                  She is instructed to reach out on the first day of the rental period to arrange
+                  pick up and drop off details.
                 </p>
-                <li>What is the Swoptop fee for the Owner? </li>
-                <p>15% of the transaction</p>
-                <li>How do I know I’ll get an item back?</li>
+              </div>
+              <div className={css.col}>
+                <h2>How do I accept or deny a rental request?</h2>
+                <p>
+                  Click on your profile picture in the top righthand corner and click “your
+                  listings”. From there you can control which listings you accept or deny. We
+                  recommend responding to a request within 24 hours out of courtesy to the Renter.
+                </p>
+                <h2>How do I know I will get my item back?</h2>
                 <p>
                   We hold our renters to high standards, which is why we designed a system that
                   holds users accountable. If a Renter does not return an item, the user will be
@@ -142,22 +134,8 @@ const FAQs = () => {
                   will no longer be able to rent. We expect all items to be treated as if they are
                   your own.
                 </p>
-                <li>How do I know the renter is reliable?</li>
-                <p>
-                  We take the security of our platform very seriously, therefore we ask you to
-                  qualify if the exchange has been done in a timely manner and if the item is
-                  returned in good condition via email. We monitor our users based on those
-                  qualifications and more. If anyone is not up to the standards of our platform they
-                  will be removed from the platform.
-                </p>
-              </ul>
+              </div>
             </div>
-            <hr />
-
-            <p style={{ textAlign: 'center' }}>
-              Didn’t find the answer you were looking for? Click “Contact Us” at the bottom of the
-              home page to get in touch.
-            </p>
           </div>
         </LayoutWrapperMain>
         <LayoutWrapperFooter>
